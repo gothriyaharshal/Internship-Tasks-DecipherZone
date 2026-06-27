618604bbc3b6a60bcf50952e2f032fb04381ea37
Harshal Gothriya <gothriyaharshal14@gmail.com>
2026-06-27 17:16:33 +0530

    Tight and loosely coupling

--- Week-02/Day-08-java_All_Oop_Programm/Programms/Animal.java
+++ Week-02/Day-08-java_All_Oop_Programm/Programms/Animal.java
@@ -1,19 +1,64 @@
-public class Animal
-{
-int a =10;
+Tight Coupling portion where if i want to create diffrent type of car which gonna be made from diffrent egine then i have to create many class and many engine 
 
-public static void main(String[] args)
-{
-  Animal a = new Animal();
-  a.display();
+
+class PetrolEngine {
+
+    void start() {
+        System.out.println("Petrol engine started");
+    }
 }
 
+class ElectricEngine {
+
+    void start() {
+        System.out.println("Electric engine started");
+    }
 }
-class Dog extends Animal
-{
-  void display()
- {
- System.out.println(a);
- }
+
+
+class PetrolCar {
+
+    PetrolEngine engine = new PetrolEngine();  // tight coupling
+
+    void drive() {
+        engine.start();
+        System.out.println("Petrol car is running");
+    }
 }
+
+
+class ElectricCar {
+
+    ElectricEngine engine = new ElectricEngine(); // tight coupling
+
+    void drive() {
+        engine.start();
+        System.out.println("Electric car is running");
+    }
+}
+
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        PetrolCar car1 = new PetrolCar();
+        car1.drive();
+
+        ElectricCar car2 = new ElectricCar();
+        car2.drive();
+    }
+}
+
+>>>>>>
+
+But in loose coupling i create only one car class and one engine inteface ....and makes diffrenct classes of Engine that i want like 
+
+i want pertrolEngine then i extend Engine interface our there and 
+implements method and if i want electricEngine then also do same
+
+and passes in its constructor
+
+
+
 
