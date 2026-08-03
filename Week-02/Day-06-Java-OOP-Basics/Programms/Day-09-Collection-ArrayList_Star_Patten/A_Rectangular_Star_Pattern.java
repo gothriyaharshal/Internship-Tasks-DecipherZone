54aa3d0bb7a1a068817d0379f653aceed4fe8188
Harshal Gothriya <gothriyaharshal14@gmail.com>
2026-08-03 16:04:11 +0530

    MultiThreadig notes

--- Week-02/Day-06-Java-OOP-Basics/Programms/Day-09-Collection-ArrayList_Star_Patten/A_Rectangular_Star_Pattern.java
+++ Week-02/Day-06-Java-OOP-Basics/Programms/Day-09-Collection-ArrayList_Star_Patten/A_Rectangular_Star_Pattern.java
@@ -1,34 +1,30 @@
-import  java.util.*;
+import java.util.*;
 
 public class A_Rectangular_Star_Pattern {
     public static void main(String[] args) {
 
-        Scanner scanner  = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
         List<String> list = new ArrayList<>();
-
 
 
         int row = scanner.nextInt();
         int column = scanner.nextInt();
 
 
-        for(int i = 1 ; i<=row ; i++)
-        {
-            for(int j = 1 ; j<=column ; j++)
-            {
+        for (int i = 1; i <= row; i++) {
+            for (int j = 1; j <= column; j++) {
                 list.add("*");
             }
         }
 
         Iterator<String> itr = list.iterator();
-          int count  = 0;
-        while(itr.hasNext()) {
+        int count = 0;
+        while (itr.hasNext()) {
             System.out.print(itr.next() + " ");
             count++;
-            if(count==column)
-            {
+            if (count == column) {
                 System.out.println();
-                count=0;
+                count = 0;
             }
         }
 
